76d3ed59d1a10e04b5449beba9f542ffca42581c
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-06-19 22:25:27 +0900

    created model

---
+++ my_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+	title = models.CharField(max_length=30)
+	complate = models.BooleanField()
+	content = models.TextField(max_length=200)
+	created_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"{self.title}"
---
+++ my_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+	path('',views.simple_view, name='simple_view')
+]
+
